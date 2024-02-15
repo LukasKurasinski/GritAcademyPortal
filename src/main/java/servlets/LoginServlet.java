1e1d527df5053e655dcdc2dfa2ccb0864d7ebf67
LukasKurasinski <146763137+LukasKurasinski@users.noreply.github.com>
2024-02-15 22:24:44 +0100

    Initial file structure. Started working on index and login pages with some styling

---
+++ src/main/java/servlets/LoginServlet.java
@@ -0,0 +1,27 @@
+package servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //System.out.println();
+
+        req.getRequestDispatcher("JSP/login.jsp").forward(req,resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+            //TODO check username and password in DB
+            //if correct do something
+            //if not correct return to login page with message
+    }
+}
---
+++ src/main/webapp/JSP/fragments/loginForm.jsp
@@ -0,0 +1,7 @@
+<form id="loginForm" action="/login" method="post">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" required>
+    <input type="submit" id="loginSubmit" name="loginSubmit">
+</form>
